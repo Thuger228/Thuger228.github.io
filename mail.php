0f145ba8f0363d10eccd909a53b0d78942e00992
Thuger228 <xupypg99@gmail.com>
2017-12-18 18:14:47 +0200

    first commit

--- mail.php
+++ mail.php
@@ -7,4 +7,10 @@ $recipient = "kindrom228@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $formcontent, $mailheader) or die("Error!");
 header("location: thank-you.html")
+error_page 405 =200 @405; 
+
+location @405 { 
+	root /htdocs; 
+	proxy_pass http://localhost:8080; 
+}
 ?>
